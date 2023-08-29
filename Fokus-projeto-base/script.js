f7ad3f9726574a2c4172a4cdd45061fee9d08254
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-08-29 17:43:19 -0300

    Inicio aula 2

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -1,16 +1,27 @@
 const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo')
+const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
+
+banner.src = './imagens/foco.png'
 
 focoBt.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'foco')
+   alteraContexto('foco')
 })
 
 curtoBt.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'descanso-curto')
+   alteraContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'descanso-longo')
+  alteraContexto('descanso-longo')
 })
+
+function alteraContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+
+    switch 
+}
